1a06f301a00857d2e50bbd28b708ba4ffc8e9349
github-AKZhuk <lesha.zhuk@gmail.com>
2021-02-15 16:09:44 +0300

    feat: impliment tasks typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+exports.min = function min(array) {
+    if (array == undefined) return 0;
+    return  result = (array.length == 0)? 0 : Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined) return 0;
+    return  result = (array.length == 0)? 0 : Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined) return 0;
+    return  result = (array.length == 0)? 0 :  array.reduce((avg, current) => avg + current) / array.length;
+};
